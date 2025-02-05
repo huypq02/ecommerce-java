298a753d8a9132fb106ff3f5995515d1245d108c
phamnga-223 <pnga2231997@gmail.com>
2025-02-05 23:25:45 +0700

    feat: Login with OAuth2 (Google)

--- src/main/java/com/cybersoft/ecommerce/service/AuthServiceImp.java
+++ src/main/java/com/cybersoft/ecommerce/service/AuthServiceImp.java
@@ -4,10 +4,13 @@ import com.cybersoft.ecommerce.repository.AuthRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.http.ResponseEntity;
 import org.springframework.http.client.HttpComponentsClientHttpRequestFactory;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 
+import java.io.UnsupportedEncodingException;
+import java.net.URLDecoder;
 import java.util.Map;
 
 @Service
@@ -15,6 +18,7 @@ public class AuthServiceImp implements AuthService {
     @Autowired
     private AuthRepository authRepository;
 
+    // Oauth2 Facebook
     @Value("${spring.security.oauth2.client.registration.facebook.client-id}")
     private String clientId;
     @Value("${spring.security.oauth2.client.registration.facebook.client-secret}")
@@ -30,6 +34,20 @@ public class AuthServiceImp implements AuthService {
     @Value("${spring.security.oauth2.client.registration.facebook.user-info-uri}")
     private String userInfoUri;
 
+    // Oauth2 Google
+    @Value("${spring.security.oauth2.client.registration.google.client-id}")
+    private String googleClientId;
+    @Value("${spring.security.oauth2.client.registration.google.client-secret}")
+    private String googleClientSecret;
+    @Value("${spring.security.oauth2.client.registration.google.redirect-uri}")
+    private String googleRedirectUri;
+    @Value("${spring.security.oauth2.client.registration.google.auth-uri}")
+    private String googleAuthUri;
+    @Value("${spring.security.oauth2.client.registration.google.token-uri}")
+    private String googleTokenUri;
+    @Value("${spring.security.oauth2.client.registration.google.user-info-uri}")
+    private String googleUserInfoUri;
+
     @Override
     public Map<String, Object> authenticateAndFetchProfile(String code, String loginType) {
         RestTemplate restTemplate = new RestTemplate();
@@ -37,9 +55,10 @@ public class AuthServiceImp implements AuthService {
         loginType = loginType.toLowerCase();
         String accessToken = "";
         String userInfo = "";
+        String url = "";
         switch (loginType) {
             case "facebook":
-                String url = tokenUri
+                url = tokenUri
                         + "?client_id=" + clientId
                         + "&redirect_uri=" + redirectUri
                         + "&client_secret=" + clientSecret
@@ -50,6 +69,16 @@ public class AuthServiceImp implements AuthService {
                 break;
             case "google":
                 // Call Google API to get user profile
+                Map<String, String> request = Map.of(
+                        "client_id", googleClientId, "redirect_uri", googleRedirectUri,
+                        "client_secret", googleClientSecret, "code", code,
+                        "grant_type", "authorization_code"
+                );
+                ResponseEntity res = restTemplate.postForEntity(googleTokenUri, request, Map.class);
+
+                String body = res.getBody().toString();
+                accessToken = body.split(",")[0].replace("{access_token=", "");
+                userInfo = googleUserInfoUri + "?access_token=" + accessToken;
                 break;
             default:
         }
@@ -65,7 +94,7 @@ public class AuthServiceImp implements AuthService {
                 url = authUri + "?client_id=" + clientId + "&redirect_uri=" + redirectUri + "&scope=email%20public_profile" + "&response_type=code" + "&loginType=" + loginType;
                 break;
             case "google":
-                url = "https://accounts.google.com/o/oauth2/v2/auth?client_id=YOUR_CLIENT_ID&redirect_uri=YOUR_REDIRECT_URI&scope=email&response_type=code";
+                url = googleAuthUri + "?client_id=" + googleClientId + "&redirect_uri=" + googleRedirectUri + "&scope=email" + "&response_type=code" + "&loginType=" + loginType;
                 break;
             default:
         }
