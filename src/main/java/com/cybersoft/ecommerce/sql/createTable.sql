ac3438adfba6e69eb7e4c963bfeadf77cdb46d9c
phamnga-223 <pnga2231997@gmail.com>
2025-02-16 23:05:05 +0700

    SQL change

--- src/main/java/com/cybersoft/ecommerce/sql/createTable.sql
+++ src/main/java/com/cybersoft/ecommerce/sql/createTable.sql
@@ -165,3 +165,29 @@ FOREIGN KEY (user_info_id)
 REFERENCES user_info(id)
 ON DELETE CASCADE
 ON UPDATE CASCADE;
+
+ALTER TABLE cart DROP CONSTRAINT FK_user_info_cart;
+ALTER TABLE cart DROP COLUMN user_info_id;
+ALTER TABLE cart ADD user_id int;
+ALTER TABLE cart
+ADD CONSTRAINT FK_user_id_cart
+FOREIGN KEY (user_id)
+REFERENCES users(id)
+ON DELETE CASCADE
+ON UPDATE CASCADE;
+
+ALTER TABLE orders DROP CONSTRAINT FK_user_info_order;
+ALTER TABLE orders DROP COLUMN user_info_id;
+ALTER TABLE orders ADD user_id int;
+ALTER TABLE orders ADD CONSTRAINT FK_user_id_orders
+FOREIGN KEY (user_id) REFERENCES users(id);
+ALTER TABLE orders ADD (
+	full_name varchar(100),
+	address varchar(255),
+	phone varchar(10),
+	postal_code varchar(10),
+	city varchar(50),
+	country varchar(50),
+	province varchar(50),
+	apt varchar(100)
+);
