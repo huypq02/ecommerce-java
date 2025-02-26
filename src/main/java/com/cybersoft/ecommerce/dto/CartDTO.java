8481e20a2d7f97198144ab8abf3dc13b378397ca
Huy Pham <huypham0297@gmail.com>
2025-02-26 17:06:36 +0700

    update add field for cart

--- src/main/java/com/cybersoft/ecommerce/dto/CartDTO.java
+++ src/main/java/com/cybersoft/ecommerce/dto/CartDTO.java
@@ -6,9 +6,11 @@ import java.util.List;
 
 @Data
 public class CartDTO {
+    private int productDetailId;git
     private String productName;
     private int quantity;
     private String size;
     private double price;
     private List<String> imageUrls;
+    private String color;
 }
